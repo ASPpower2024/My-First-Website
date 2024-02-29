a0ebb4f82d76be9a7b84b02d704b4eb1c5b95fc4
Anthony Phillips <anthony.phillips@powerhrg.com>
2024-02-29 09:40:43 -0500

    updated Nav links...button addtion and size

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript"
+h2.textContent = "Enemies to Use Against"
 
 document.querySelector("body").appendChild(h2);
