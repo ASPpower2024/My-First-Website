4ebb1ebc064da109409ac9257791cbc367141794
Anthony Phillips <anthony.phillips@powerhrg.com>
2024-03-01 02:36:28 -0500

    Event lister on main picture

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ h2.textContent = "Enemies to Use Against"
 
 document.querySelector("body").appendChild(h2);
 
+const input = document.getElementById('myImage');
+
+function clickAlert() {
+  alert('EVERYTHING IS ON FIRE!');
+}
+
+input.addEventListener('click', clickAlert);
